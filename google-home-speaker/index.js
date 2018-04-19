a8fac99a7f0ffe9263b8b655a6ce2bada426dadc
kei-s <kei.shiratsuchi@gmail.com>
2018-04-19 17:41:24 +0900

    Add google home speaker

---
+++ google-home-speaker/index.js
@@ -0,0 +1,35 @@
+let express = require('express');
+let googlehome = require('google-home-notifier');
+let app = express();
+
+let credentials = require('./credentials.json');
+
+app.get('/speak', (req, res) => {
+  console.log(req.query);
+  let text = req.query.text;
+
+  if (credentials.deviceName) {
+    googlehome.device(credentials.deviceName, credentials.language);
+  } else {
+    googlehome.ip(credentials.ip, credentials.language);
+  }
+
+  if (!text) {
+    res.send('Request: GET /speak?text=こんにちは');
+  }
+
+  try {
+    googlehome.notify(text, (notifyRes) => {
+      console.log(notifyRes);
+      res.send(credentials.deviceName + ' will say: ' + text + '\n');
+    });
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+    res.send(err);
+  }
+});
+
+let server = app.listen(credentials.port, () => {
+  console.log('Server started on: %s:%s', server.address().address, server.address().port);
+});
